a3f46db19496d2f49d88aaa742bb3102923712f3
Valentina <valentinarp54@gmail.com>
2026-05-15 00:09:50 -0500

    DBCONECTION

---
+++ ChisteConsumerAPI/Controllers/ChisteControllerCOnsumer.cs
@@ -0,0 +1,22 @@
+﻿using ChisteConsumerAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChisteConsumerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class JokesController : ControllerBase
+    {
+        private readonly ChisteDbContext _context;
+
+        public JokesController(ChisteDbContext context) => _context = context;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllJokes()
+        {
+            var jokes = await _context.Jokes.ToListAsync();
+            return Ok(jokes);
+        }
+    }
+}
